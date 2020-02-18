453b4b465928e64d7c94f764816d7f28bd847971
sousuke0422 <sousuke20xx@gmail.com>
2020-02-18 21:26:18 +0900

    ##

--- windows.bat
+++ windows.bat
@@ -1,4 +1,4 @@
 @echo off
 powershell Write-Host "新しいオープンソースのpowershell'('>=7')'が必要です。しかし、=7はプレビュー版のため、=6で動くように機能制限中"
 rem pwsh -Command { if(where.exe wsl){Write-Host "found 'wsl'" && Write-Host "お使いのwslで動作可能です。"}Else{Write-Host "not found 'wsl'"} }
-if(where.exe wsl){Write-Host "found 'wsl'"}Else{Write-Host "not found 'wsl'"}
+pwsh -Command if(where.exe wsl){Write-Host "found 'wsl'"}Else{Write-Host "not found 'wsl'"}
