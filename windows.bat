75b0fc7adc1986207875d95e27b20bf05d92dc63
sousuke0422 <sousuke20xx@gmail.com>
2020-02-18 21:07:39 +0900

    修正
    &&は7からなのか・・・

--- windows.bat
+++ windows.bat
@@ -1,2 +1,3 @@
 @echo off
-powershell if(where wsl){Write-Host "found 'wsl'" && Write-Host "お使いのwslで動作可能です。"}Else{Write-Host "not found 'wsl'"}
+powershell Write-Host "新しいオープンソースのpowershell(>=7)が必要です。"
+pwsh if(where wsl){Write-Host "found 'wsl'" && Write-Host "お使いのwslで動作可能です。"}Else{Write-Host "not found 'wsl'"}
